212f019caf8967f7beefd01450074a6fb5148708
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-11-06 13:08:30 -0500

    make number guessable

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
+/*----- constants -----*/
 const game = {
   title: 'Guess the Number!',
-  getBigNum: function() {
-    let bigNum = 100;
-    return bigNum;
-  },
-  getSmallNum: function() {
-    let smallNum = 1;
-    return smallNum;
-  },
-  secretNum: null,
-  prevGuesses: [],
-  play: function() {
-    let bigNum = this.getBigNum();
-    let smallNum = this.getSmallNum();
-    this.secretNum = Math.floor(Math.random() *
-      (bigNum - smallNum + 1)) + smallNum;
-    console.log(this.secretNum);
-  },
 };
 
-game.play();
+/*----- app's state -----*/
+game.guess = 0;
 
-const form = document.getElementById('form');
-const message = document.getElementById('message');
-const guess = document.getElementById('guess');
+game.getBigNum = function() {
+  let bigNum = 100;
+  return bigNum;
+};
 
-form.addEventListener('submit', function(evt) {
+game.getSmallNum = function() {
+  let smallNum = 1;
+  return smallNum;
+};
+
+game.play = function() {
+  let bigNum = this.getBigNum();
+  let smallNum = this.getSmallNum();
+  this.secretNum = Math.floor(Math.random() *
+    (bigNum - smallNum + 1)) + smallNum;
+  console.log(this.secretNum);
+};
+
+game.secretNum = null;
+game.guess = 0;
+game.prevGuesses = [];
+
+/*----- cached element references -----*/
+const formEl = document.getElementById('form');
+const messageEl = document.getElementById('message');
+const guessSubEl = document.getElementById('guess');
+
+/*----- event listeners -----*/
+formEl.addEventListener('submit', function(evt) {
   evt.preventDefault();
-  showMessage();
+  game.guess = parseInt(guessSubEl.value)
+  takeTurn();
+  render();
 });
 
-function showMessage() {
-  let newText = guess.value;
-  message.textContent = newText;
+/*----- functions -----*/
+game.play();
+
+function takeTurn() {
+  game.prevGuesses.push(game.guess);
+  console.log(game.prevGuesses);
+}
+
+function render() {
+  if (game.guess === game.secretNum) {
+    message.textContent = `Congrats! You guessed the number in ${game.prevGuesses.length} guesses!`;
+  }
 }
